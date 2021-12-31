b316dc54d828a0366c5dd3f89646078739a79d14
envity <1801040191@s.hanu.edu.vn>
2022-01-01 00:26:36 +0700

    Update Setting,SettingDAO

--- src/main/java/com/FMS/app/file/dao/SettingDAO.java
+++ src/main/java/com/FMS/app/file/dao/SettingDAO.java
@@ -1,5 +1,61 @@
 package com.FMS.app.file.dao;
 
-public class SettingDAO {
+import com.FMS.app.file.model.Setting;
+import com.FMS.app.util.DbConnector;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
+public class SettingDAO implements Dao<Setting>{
+    private Connection conn;
+
+    public SettingDAO(DbConnector connector) {
+      this.conn = connector.getConnection();
+    }
+
+    @Override
+    public List<Setting> getAll() {
+        List<Setting> settings = new ArrayList<>();
+        try {
+            Statement stmt = this.conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from setting");
+
+            while (rs.next()) {
+              Setting setting = new Setting(
+                  rs.getInt(1),
+                  rs.getInt(2),
+                  rs.getInt(3),
+                  rs.getString(4),
+                  rs.getDate(5));
+                settings.add(setting);
+            }
+          } catch (Exception e) {
+            System.err.println("[ERROR] " + e.getMessage());
+          }
+          return settings;
+    }
+
+    @Override
+    public Optional<Setting> get(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void save(Setting t) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void update(Setting t) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void delete(Setting t) {
+        // TODO Auto-generated method stub
+    }
 }
--- src/main/java/com/FMS/app/file/model/Setting.java
+++ src/main/java/com/FMS/app/file/model/Setting.java
@@ -1,5 +1,44 @@
 package com.FMS.app.file.model;
 
+import java.sql.*;
+
 public class Setting {
-  // TODO
+    private int id;
+    private int maxFileSize;
+    private int itemPerPage;
+    private String mimeTypeAllowed;
+    private Date lastUpdatedTime;
+
+    public Setting(
+      int id,
+      int maxFileSize,
+      int itemPerPage,
+      String mimeTypeAllowed,
+      Date lastUpdatedTime) {
+    this.id = id;
+    this.maxFileSize = maxFileSize;
+    this.itemPerPage = itemPerPage;
+    this.mimeTypeAllowed = mimeTypeAllowed;
+    this.lastUpdatedTime = new Date(System.currentTimeMillis());
+  }
+
+public int getId() {
+  return this.id;
+}
+
+public int getMaxFileSize() {
+  return this.maxFileSize;
+}
+
+public int getItemPerPage() {
+  return this.itemPerPage;
+}
+
+public String getMimeTypeAllowed() {
+  return this.mimeTypeAllowed;
+}
+
+public Date getLastUpdatedTime() {
+  return this.lastUpdatedTime;
+}
 }
