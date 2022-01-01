57b3432d1594633127b036e8cce6c236e80f3f7c
hungpq96 <quanghwng22@gmail.com>
2022-01-01 18:05:48 +0700

    Add controller boilerplate

--- src/main/java/com/FMS/app/App.java
+++ src/main/java/com/FMS/app/App.java
@@ -1,42 +1,11 @@
 package com.FMS.app;
 
-import static spark.Spark.*;
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import com.FMS.app.file.dao.*;
-import com.FMS.app.file.model.*;
-import com.FMS.app.util.*;
+import com.FMS.app.file.controller.FileController;
+import com.FMS.app.file.controller.SettingController;
 
 public class App {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            // sample database retrieve
-            DbConnector connector = new DbConnector();
-            FileDao fileDao = new FileDao(connector);
-            Optional<File> xxx = fileDao.get(1);
-            xxx.get().setFileName("bar.jpg");
-            fileDao.update(xxx.get());
-            try {
-                List<File> f = fileDao.getAll();
-                for (File l : f) {
-                    System.out.println(l);
-                }
-            } catch (Exception e) {
-                System.err.println("[ERROR] " + e.getMessage());
-            }
-
-            return "Hello Worldsddfsdf";
-        });
-
-        get("/sample_view", (req, res) -> {
-            Map m = new HashMap();
-            m.put("key", "value");
-            // sample.hbs will be understand as src/main/resources/sample.hbs
-            return new ModelAndView(m, "sample.hbs");
-        }, new HandlebarsTemplateEngine());
+        FileController.load();
+        SettingController.load();
     }
 }
---
+++ src/main/java/com/FMS/app/file/controller/SettingController.java
@@ -0,0 +1,18 @@
+package com.FMS.app.file.controller;
+
+import static spark.Spark.post;
+
+import com.FMS.app.file.dao.SettingDao;
+import com.FMS.app.file.model.Setting;
+
+public class SettingController extends Controller {
+  private static SettingDao settingDao = new SettingDao(Controller.connector);
+
+  public static void load() {
+    post("/update_settings", (req, res) -> {
+      // TODO: update first setting
+      res.redirect("/");
+      return null;
+    });
+  }
+}
