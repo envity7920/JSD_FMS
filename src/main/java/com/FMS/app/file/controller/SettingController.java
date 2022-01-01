d71cce7fcf6e0790667081afb23983cee775379d
envity <1801040191@s.hanu.edu.vn>
2022-01-02 03:26:32 +0700

    Commit for pulling delete version

--- src/main/java/com/FMS/app/file/controller/SettingController.java
+++ src/main/java/com/FMS/app/file/controller/SettingController.java
@@ -2,6 +2,8 @@ package com.FMS.app.file.controller;
 
 import static spark.Spark.post;
 
+import java.sql.Timestamp;
+
 import com.FMS.app.file.dao.SettingDao;
 import com.FMS.app.file.model.Setting;
 
@@ -11,10 +13,18 @@ public class SettingController extends Controller {
   public static void load() {
     post("/update_settings", (req, res) -> {
       // TODO: update first setting
-       Setting t =  new Setting(maxFileSize, itemPerPage, mimeTypeAllowed, lastUpdatedTime)
+      String newMaxFileSize;
+      String newItemPerPage;
+      String newAllowedUploadType;
 
+      newMaxFileSize = req.queryParams("maxFileSizeInput");
+      newItemPerPage = req.queryParams("itemPerPageInput");
+      newAllowedUploadType = req.queryParams("selectedMimeType");
+
+      Setting t = new Setting(Integer.parseInt(newMaxFileSize), Integer.parseInt(newItemPerPage), newAllowedUploadType, new Timestamp(System.currentTimeMillis()));
+      settingDao.save(t);
       res.redirect("/");
-      return null;
+      return res;
     });
   }
 }
